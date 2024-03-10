c8ed63841079e11acce3be62d075a882e7f92160
Vedat Edip Özayar <43388269+vedatozayar@users.noreply.github.com>
2024-03-10 19:38:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
+    </>
+  );
+}
+
+export default App;
